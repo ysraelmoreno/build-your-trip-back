1fd6e27bcede5960ca6fcf53c85dac6aba198df8
Ysrael Moreno <ysraelmoreno02@gmail.com>
2022-01-14 15:16:04 -0300

    feat(env): add environments key

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,14 @@
 import makeConnection from "./connection/makeConnection";
+import "dotenv/config";
 
 console.log("Starting database");
 
 makeConnection({
-  host: "localhost",
-  port: 5432,
-  user: "root",
-  password: "root",
-  database: "build_your_trip",
+  host: process.env.BYT_HOST || "localhost",
+  port: Number(process.env.BYT_PORT) || 5432,
+  user: process.env.BYT_USER || "root",
+  password: process.env.BYT_PASSWORD || "root",
+  database: process.env.BYT_DATABASE || "build_your_trip",
   type: "postgres",
   name: "default",
 });
